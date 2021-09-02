dea9288c82133cb0cc3009138ffc99565325e531
Jenoah Kers <hello@jenoah.nl>
2021-09-02 16:20:59 +0200

    Added reload functionality on timer

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -6,6 +6,29 @@ using UnityEngine.UI;
 public class PlayerHealth : EntityHealth
 {
     [SerializeField] private Image healthBar = null;
+    [SerializeField] private KeyCode reloadKey = KeyCode.R;
+    [SerializeField] private float reloadCooldown = 15f;
+
+    private float nextReloadTime = 0;
+
+    private void Start()
+    {
+        startHealth = health;
+        nextReloadTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(reloadKey))
+        {
+            if (Time.time > nextReloadTime)
+            {
+                health = startHealth;
+                if (healthBar != null) healthBar.fillAmount = 1f / (startHealth / health);
+                nextReloadTime = Time.time + reloadCooldown;
+            }
+        }
+    }
 
     public override void DealDamage(float damageAmount)
     {
