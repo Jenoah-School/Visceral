e5d4652705caa97dafabbd91bd3c2948e782d6ad
Jenoah Kers <hello@jenoah.nl>
2021-09-14 14:18:44 +0200

    Fixed game-breaking bug and made game loop

---
+++ Assets/Scripts/Debugging/Debug message.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugmessage : MonoBehaviour
+{
+    public void DebugMessage(string message)
+    {
+        Debug.Log(message);
+    }
+}
